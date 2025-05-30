81d191f3acaaf469d260c95c082454ba7c7bd89f
Manasés Turcios Ortéz <manasesortez1234@hotmail.com>
2025-05-30 09:43:34 -0600

    admin chage applications-projects

--- src/app/public/components/pages/project/project-detail/project-detail.component.ts
+++ src/app/public/components/pages/project/project-detail/project-detail.component.ts
@@ -17,11 +17,11 @@ export class ProjectDetailComponent implements OnInit {
   error: string | null = null;
 
   statusOptions = [
-    { label: 'Pendiente', value: 0 },
+    //{ label: 'Pendiente', value: 0 },
     { label: 'Activo', value: 1 },
     //{ label: 'Inactivo', value: 2 },
     { label: 'Rechazado', value: 3 },
-    { label: 'Completado', value: 4 }
+    //{ label: 'Completado', value: 4 }
   ];
 
   selectedStatus!: number;
